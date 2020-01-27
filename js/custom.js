b6303de845e980a8e9e69d85041fd2b43a451ad8
Edgar Bongkishiy <biz@bongkishiy.de>
2020-01-27 19:50:37 +0100

    Added new javascript for protecting email from spam

---
+++ js/custom.js
@@ -0,0 +1,5 @@
+function gen_bewerben_mail_to_link(lhs,rhs,subject) {
+    document.write("<a class='btn-link' style='color: darkblue' href=\"mailto");
+    document.write(":" + lhs + "@");
+    document.write(rhs + "?subject=" + subject + "\">" + lhs + "@" + rhs + "<\/a>");
+}
