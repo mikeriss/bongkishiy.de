a04bd2619b0c01348e66e0873f83588bd14f9b04
Edgar Bongkishiy <biz@bongkishiy.de>
2020-01-28 18:46:13 +0100

    made email method more global

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-function gen_bewerben_mail_to_link(lhs,rhs,subject) {
+function gen_mail_to_link(lhs,rhs,subject) {
     document.write("<a class='btn-link' style='color: darkblue' href=\"mailto");
     document.write(":" + lhs + "@");
     document.write(rhs + "?subject=" + subject + "\">" + lhs + "@" + rhs + "<\/a>");
